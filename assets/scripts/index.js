70fa974d8a29d74475db103bedcb876cffd13d54
sree-r-one <sreeraj_ec@hotmail.com>
2024-06-08 01:43:53 +0800

    commit of the dat

---
+++ assets/scripts/index.js
@@ -0,0 +1,7 @@
+import { apiKey } from "./util.js";
+import default_export from "./util.js";
+
+const elem = document.getElementById("element");
+
+elem.innerHTML = "testing ";
+elem.innerHTML += default_export;
---
+++ assets/scripts/util.js
@@ -0,0 +1,2 @@
+export let apiKey = "12341swrserw233234";
+export default "the default export";
